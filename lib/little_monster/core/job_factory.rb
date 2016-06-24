065685543a90eb536ddfe169dd6283ff07a0bdac
Sebastian Sujarchuk <sebastian.sujarchuk@mercadolibre.com>
2016-06-24 14:26:47 -0300

    job factory fetch is critical

--- lib/little_monster/core/job_factory.rb
+++ lib/little_monster/core/job_factory.rb
@@ -29,7 +29,9 @@ module LittleMonster::Core
 
     def fetch_attributes
       return {} if %w(development test).include? LittleMonster.env
-      resp = API.get "/job/#{@id}"
+      resp = API.get "/job/#{@id}", retries: LittleMonster.job_requests_retries,
+                                    retry_wait: LittleMonster.job_requests_retry_wait,
+                                    critical: true
 
       if resp.success?
         resp.body

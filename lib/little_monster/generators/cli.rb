8847d64291f952515b08659d47db4bab84042a3f
Hora b <horaciob@gmail.com>
2016-06-26 16:15:18 -0300

    thor

---
+++ lib/little_monster/generators/cli.rb
@@ -0,0 +1,32 @@
+require 'byebug'
+require 'thor'
+
+require_relative './generator'
+
+module LittleMonster
+  class Cli < Thor
+    desc 'version','version'
+    map %w[-v --version] => :version
+    def version
+      say '0.0.0'
+    end
+
+    desc 'generate', 'generate a new job'
+    map %w[g --generate] => :generate
+
+    argument :job_name, 
+      type: :string, 
+      banner: 'Job Name',
+      required: true
+
+    argument :task_names, 
+      type: :array, 
+      banner: 'A set of instructions for making or preparing something',
+      required: true
+
+    def generate
+      #generator.destination_root = '/tmp'
+      invoke Generator
+    end
+  end
+end
---
+++ lib/little_monster/generators/generator.rb
@@ -0,0 +1,49 @@
+require 'byebug'
+require 'thor'
+require "active_support/all"
+
+module LittleMonster
+  class Generator < Thor::Group
+    include Thor::Actions
+
+
+    argument :job_name, 
+      type: :string, 
+      banner: 'Job Name',
+      required: true
+
+    argument :task_names, 
+      type: :array, 
+      banner: 'A set of instructions for making or preparing something',
+      required: true
+
+
+    class_option :enviroment, :default => :development
+
+    def self.source_root
+      File.dirname(__FILE__)
+    end
+
+    def create_job_file
+      template('templates/jobs_temp.erb', "jobs/#{job_name}.rb")
+    end
+
+    def create_tasks_file
+      task_names.each do |task|
+        @current_task_name=task
+        template('templates/tasks_temp.erb', "tasks/#{job_name}/#{task}.rb")
+      end
+    end
+
+    def create_job_test_file
+      template 'templates/jobs_spec_temp.erb',"spec/jobs/#{job_name}_spec.rb"
+    end
+
+    def create_tasks_test_file
+      task_names.each do |task|
+        @current_task_name=task
+        template 'templates/tasks_spec_temp.erb',"spec/tasks/#{job_name}/#{task}_spec.rb"
+      end
+    end
+  end
+end

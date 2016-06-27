4a14396af71804e031519f50e5bbc93c0a63e417
Hora b <horaciob@gmail.com>
2016-06-27 00:49:30 -0300

    add namespace

--- lib/little_monster/generators/run.rb
+++ lib/little_monster/generators/run.rb
@@ -3,14 +3,17 @@ Bundler.require(:default,:development)
 
 require_all "./jobs"
 require_all "./tasks"
-class Run < Thor
-  
-  desc 'run a task','runner'
-  argument :job, type: :string
-	def start
-		message={params:{"un_parametro":"un valor"},name: job}
-		#on_message
-		job = LittleMonster::Job::Factory.new(message).build
-		job.run unless job.nil?
-	end
+
+module LittleMonster
+  class Run < Thor
+
+    desc 'run a task','runner'
+    argument :job, type: :string
+    def start
+      message={params:{"un_parametro":"un valor"},name: job}
+      #on_message
+      job = LittleMonster::Job::Factory.new(message).build
+      job.run unless job.nil?
+    end
+  end
 end

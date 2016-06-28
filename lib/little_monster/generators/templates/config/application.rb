d0e3230914285819ad26820df6177fea0cd4f461
Hora b <horaciob@gmail.com>
2016-06-28 13:55:05 -0300

    confis

--- lib/little_monster/generators/templates/config/application.rb
+++ lib/little_monster/generators/templates/config/application.rb
@@ -4,6 +4,8 @@ Bundler.require(:default)
 LittleMonster.configure do |conf|
   conf.api_request_retries = 3
   conf.api_url = "http://pepe.hongo.com" 
+  #conf.worker_concurrency = 200
+  #conf.worker_queue = 'my_sqs_queue'
 end
 
 require_relative "enviroments/#{LittleMonster.env}"

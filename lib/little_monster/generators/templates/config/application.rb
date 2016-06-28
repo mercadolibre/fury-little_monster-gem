f73bbf7946e22b02aa1ba679fde9402f160b6e0f
Hora b <horaciob@gmail.com>
2016-06-28 12:51:09 -0300

    config

--- lib/little_monster/generators/templates/config/application.rb
+++ lib/little_monster/generators/templates/config/application.rb
@@ -2,7 +2,7 @@ require 'bundler'
 Bundler.require(:default)
 
 LittleMonster.configure do |conf|
-  #lib/little_monster.rb:25
+  conf.api_request_retries = 3
   conf.api_url = "http://pepe.hongo.com" 
 end
 

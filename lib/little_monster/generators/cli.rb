f4e59592aa8bcab69dc4da813d8d215398bf6950
Hora b <horaciob@gmail.com>
2016-07-29 18:11:57 -0300

    mocks

--- lib/little_monster/generators/cli.rb
+++ lib/little_monster/generators/cli.rb
@@ -39,12 +39,35 @@ module LittleMonster
       default: false,
       aliases: :d
 
-    def start
-      require_relative "#{Dir.pwd}/config/application.rb"
+    method_option :record_mode,
+      aliases: '-r',
+      type: :string,
+      enum: ['none','new','reload'],
+      default: 'none',
+      desc: 'Recording mocks mode  none|new|reload',
+      banner: 'Recording type could be none,new or reload on default assume none'
 
+    def start
+      ENV['LITTLE_MONSTER_ENV'] = options[:environment]
+      require 'webmock'
+      require 'vcr'
+
+      require_relative "#{Dir.pwd}/config/application.rb"
       toiler_args = ['-C', "#{Dir.pwd}/config/toiler.yml"]
       toiler_args += ['-d', '-L', 'log/little_monster.log'] if options[:daemonize]
-      Toiler::CLI.instance.run(toiler_args)
+
+      VCR.configure do |config|
+        config.cassette_library_dir = "mocks/vcr_cassettes"
+        config.hook_into :webmock # or :fakeweb
+      end 
+
+      vcr_mode = { 'none' => :none,
+                   'new' => :new_episodes,
+                   'reload' => :all }.fetch(options[:record_mode], :none)
+
+      VCR.use_cassette(job.to_s, record: vcr_mode) do
+        Toiler::CLI.instance.run(toiler_args)
+      end
     end
 
     register(LittleMonster::ConfGen, 'init', 'init','Creates new Little Monster Schema app')

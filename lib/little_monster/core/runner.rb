6fe8c99b58fe75de79bff0887a38f7bd1ded8069
Hernan Maschwitz <hernanm992@gmail.com>
2016-08-04 17:52:49 -0300

    rubocop styles

--- lib/little_monster/core/runner.rb
+++ lib/little_monster/core/runner.rb
@@ -26,12 +26,10 @@ module LittleMonster::Core
     def send_heartbeat!
       return if LittleMonster.disable_requests?
 
-      res = LittleMonster::API.put "/jobs/#{@params[:id]}/worker", {
-        body: {
-          ip: Socket.gethostname,
-          host: Socket.gethostname,
-          pid: Process.pid
-        }
+      res = LittleMonster::API.put "/jobs/#{@params[:id]}/worker", body: {
+        ip: Socket.gethostname,
+        host: Socket.gethostname,
+        pid: Process.pid
       }
 
       raise LittleMonster::JobAlreadyLockedError, "job [id:#{@params[:id]}] is already locked, discarding" if res.code == 401

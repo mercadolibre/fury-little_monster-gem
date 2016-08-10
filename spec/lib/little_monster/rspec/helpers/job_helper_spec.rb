d6f8fc25e5c675cca901e7306534a12f67811016
Sebastian Sujarchuk <sebastian.sujarchuk@mercadolibre.com>
2016-08-10 15:45:54 -0300

    add generate_job helper

--- spec/lib/little_monster/rspec/helpers/job_helper_spec.rb
+++ spec/lib/little_monster/rspec/helpers/job_helper_spec.rb
@@ -40,29 +40,45 @@ describe LittleMonster::RSpec::JobHelper do
 
 
   describe '::run_job' do
+    let(:job) { job_class.new }
+
+    before :each do
+      allow(job_class).to receive(:new).and_return(job)
+    end
+
+    it 'returns a result wrapper' do
+      expect(run_job(job_class, options).class).to eq(LittleMonster::RSpec::JobHelper::Result)
+    end
+
+    it 'has a job instance in the result wrapper' do
+      expect(run_job(job_class, options).instance).to eq(job)
+    end
+  end
+
+  describe '::generate_job' do
     context 'given a job name and hash' do
       context 'when job is a symbol' do
         it 'builds job class out of symbol' do
           allow(job_class).to receive(:new).and_call_original
-          run_job job_class.to_s.underscore, options
+          generate_job job_class.to_s.underscore, options
           expect(job_class).to have_received(:new)
         end
       end
 
       it 'calls mock! on job' do
         allow(job_class).to receive(:mock!).and_call_original
-        run_job job_class, options
+        generate_job job_class, options
         expect(job_class).to have_received(:mock!)
       end
 
       it 'builds an instance of the job' do
         allow(job_class).to receive(:new).and_call_original
-        run_job job_class.to_s.underscore, options
+        generate_job job_class.to_s.underscore, options
         expect(job_class).to have_received(:new).with(data: { outputs: options[:data] })
       end
 
       context 'instance' do
-        let(:job) { run_job(job_class, options).instance }
+        let(:job) { generate_job(job_class, options) }
 
         it 'returns cancelled true if options cancelled is true' do
           options[:cancelled] = true
@@ -86,31 +102,15 @@ describe LittleMonster::RSpec::JobHelper do
 
           it 'makes tasks fails with specified expection' do
             options[:fails] = [{ task: :task_a, error: LittleMonster::TaskError }]
-            run_job(job_class, options)
+            generate_job(job_class, options)
             expect { MockJob::TaskA.new(nil).run }.to raise_error(LittleMonster::TaskError)
           end
 
           it 'makes each job fails with standard error when no exception is provided' do
             options[:fails] = [{ task: :task_a }]
-            run_job(job_class, options)
+            generate_job(job_class, options)
             expect { MockJob::TaskA.new(nil).run }.to raise_error(StandardError)
           end
-        end
-      end
-
-      context 'data' do
-        let(:job) { job_class.new }
-
-        before :each do
-          allow(job_class).to receive(:new).and_return(job)
-        end
-
-        it 'returns a result wrapper' do
-          expect(run_job(job_class, options).class).to eq(LittleMonster::RSpec::JobHelper::Result)
-        end
-
-        it 'has a job instance in the result wrapper' do
-          expect(run_job(job_class, options).instance).to eq(job)
         end
       end
     end

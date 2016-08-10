9fb06ba948376d75c15c0c47fcf929af1eb2ead5
Valentin Paz Marcolla <valentinpazm@gmail.com>
2016-08-10 14:40:29 -0300

    JobNotFoundError tests done and passing

--- spec/lib/little_monster/core/errors/job_not_found_error_spec.rb
+++ spec/lib/little_monster/core/errors/job_not_found_error_spec.rb
@@ -5,10 +5,25 @@ describe LittleMonster::Core::JobNotFoundError do
     let(:job_id) { 23 }
     let(:error) { LittleMonster::Core::JobNotFoundError }
 
-    it 'sends status error'
+    before :each do
+      allow(LittleMonster::API).to receive(:put)
+      error.new(job_id)
+    end
 
-    it 'is a critical request'
+    it 'sends status error' do
+      expect(LittleMonster::API).to have_received(:put) do |url, params, options|
+        expect(params[:body][:status]).to eq('error')
+      end
+    end
 
-    it 'sends the put to the job_id'
+    it 'is a critical request' do
+      expect(LittleMonster::API).to have_received(:put) do |url, params, options|
+        expect(options[:critical]).to eq(true)
+      end
+    end
+
+    it 'sends the put to the job_id' do
+      expect(LittleMonster::API).to have_received(:put).with("/jobs/#{job_id}", any_args).once
+    end
   end
 end

8e343a410a97d3c6a4d6447c0fdeb60dec5ca63d
Valentin Paz Marcolla <valentinpazm@gmail.com>
2016-08-10 12:42:24 -0300

    JobNotFound sends put and tests passing and laid out

---
+++ spec/lib/little_monster/core/errors/job_not_found_error_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe LittleMonster::Core::JobNotFoundError do
+  describe '#initialize' do
+    let(:job_id) { 23 }
+    let(:error) { LittleMonster::Core::JobNotFoundError }
+
+    it 'sends status error'
+
+    it 'is a critical request'
+
+    it 'sends the put to the job_id'
+  end
+end
